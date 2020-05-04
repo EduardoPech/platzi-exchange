7784f5b2cd7edc9a1dc17e8144b2e1ff2ced806c
Eduardo Pech <eduardopech.web@gmail.com>
2020-05-03 19:12:41 -0500

    Init project exchange of Platzi

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,37 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <PxHeader :links="links" />
+    <router-view class="container px-5 sm:px-20 py-20 flex justify-center" />
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import PxHeader from '@/components/PxHeader.vue'
 
 export default {
-  name: "App",
+  name: 'App',
   components: {
-    HelloWorld
-  }
-};
+    PxHeader,
+  },
+  data() {
+    return {
+      links: [
+        {
+          title: 'BTC',
+          to: { name: 'coin-detail', params: { id: 'bitcoin' } },
+        },
+        {
+          title: 'ETH',
+          to: { name: 'coin-detail', params: { id: 'ethereum' } },
+        },
+        {
+          title: 'XRP',
+          to: { name: 'coin-detail', params: { id: 'ripple' } },
+        },
+      ],
+    }
+  },
+}
 </script>
 
 <style lang="scss">
@@ -23,6 +41,5 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
